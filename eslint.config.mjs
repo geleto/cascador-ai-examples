2f8929e1a5d80fcee2ee17d9a817c9657cdf6f12
Angel Popov <geleto@gmail.com>
2025-11-10 11:17:44 +0000

    Update ESLint configuration to disable a TypeScript rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,7 +34,8 @@ export default tseslint.config(
 			'no-multi-spaces': 'error',
 
 			"@typescript-eslint/no-explicit-any": "off",
-			"@typescript-eslint/no-non-null-assertion": "off"
+			"@typescript-eslint/no-non-null-assertion": "off",
+			"@typescript-eslint/restrict-template-expressions": "off"
 		}
 	}
 );
